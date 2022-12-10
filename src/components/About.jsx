cdba4dbd5ee90db7a877cefa810fa04d470f23ac
Toha Islam <islamtoha710@gmail.com>
2022-12-10 15:22:55 +0600

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import aboutImg from '../assets/images/about.png'
 
 const About = () => {
-  return <div>About</div>;
+  const info = [
+    { text: 'Years experience', count: '01' },
+    { text: 'Complete Projects', count: '10' },
+    { text: 'Courses Work', count: '12' },
+  ]
+  return <section id="about" className="py-10 text-white">
+    <div className="text-center mt-8">
+      <h3 className="text-4xl font-semibold">
+        About <span className="text-cyan-600">Me</span>
+      </h3>
+      <p className="text-gray-400 my-3 text-lg">My Introduction</p>
+      <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+        <div>
+          <div className="text-gray-300 my-3 ">
+            <p className="text-justify leading-7">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro impedit enim dolor accusantium natus blanditiis atque incidunt laborum officiis reiciendis deserunt fuga saepe mollitia repellat obcaecati praesentium eum nam et accusamus dolorem, perferendis architecto voluptates ea! Quod sint quos molestias obcaecati laboriosam tempore est voluptates blanditiis! Cum sapiente impedit a.
+            </p>
+            <div className="flex mt-10 items-center gap-7">
+              {
+                info.map(content => (
+                  <div key={content.text}>
+                    <h3 className="md:text-4xl text-2xl font-semibold text-white">{content.count} <span className="text-cyan-600">+</span>{""}</h3>
+                    <span>{content.text}</span>
+                  </div>
+                ))
+              }
+            </div>
+            <br />
+            <br />
+            <a href="./src/assets/My Resume.pdf" download>
+              <button className="btn-primary">
+                Download Resume
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg">
+            <img src={aboutImg} alt="" className="w-full object-cover bg-cyan-600 rounded-xl" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>;
 };
 
 export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import hero from '../assets/images/hero.png'
 
 const Hero = () => {
-  return <div>Hero</div>;
+
+  return <section id="home" className="min-h-screen flex py-10 md:flex-row flex-col items-center">
+    <div className="flex-1 flex items-center justify-center h-full">
+      <img src={hero} alt="" className="md:w-11/12 h-full object-cover" />
+    </div>
+    <div className="flex-1">
+      <div className="md:text-left text-center">
+        <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
+          <span className="text-cyan-600 md:text-6xl text-5xl ">
+            Hello !
+            <br />
+          </span>
+          My Name Is <span>Toha Islam</span>
+        </h1>
+        <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-600">
+          Front End Developer
+        </h4>
+        <button className="btn-primary mt-8">Contact Me</button>
+        <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
+          <a href="https://www.instagram.com/tohaislam8920/"><ion-icon name="logo-instagram"></ion-icon></a>
+          <a href="https://www.facebook.com/irk.toha"><ion-icon name="logo-facebook"></ion-icon></a>
+          <a href="https://www.linkedin.com/in/tohaislam/"><ion-icon name="logo-linkedin"></ion-icon></a>
+          <a href="https://github.com/Toha8920/"><ion-icon name="logo-github"></ion-icon></a>
+        </div>
+      </div>
+    </div>
+  </section>;
 };
 
 export default Hero;
